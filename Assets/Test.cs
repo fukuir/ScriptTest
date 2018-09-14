1e001161943ba26cb741a7c95e5ff7b28c1ec289
FukuiRyoutarou <calicocatff37@icloud.com>
2018-09-14 23:50:41 +0900

    ScriptTest
    配列を順番通りに表示
    for文で逆順に表示

---
+++ Assets/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < array.Length; i++)
+            Debug.Log(array[i]);
+        for (int i = 4; i >= 0; i--)
+            Debug.Log(array[i]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
